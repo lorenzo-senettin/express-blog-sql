b4e82d57fa163fdd05f41513370929b0a93cd4f2
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-04-21 15:30:33 +0200

    add post router to server setup

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const db = require("./data/db.js");
+const db = require("./data/db");
+const postRouter = require("./routers/postRouter");
 
 const app = express();
+
 app.use(express.json());
+app.use("/api", postRouter);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server avviato sulla porta ${PORT}`);
 });
